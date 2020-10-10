1858b508b91609a5cb5a49152b1605026df16859
NickBlade <javiadamrius@gmail.com>
2020-10-10 14:54:23 +0200

    Added dependencies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
-const app = express()
+const app = express();
 
 MongoClient.connect("mongodb://localhost:27017/ElecomWebServer", {useUnifiedTopology: true}, function(err, db) {
   if(!err) {
