4c9bace5b8911c4b561438626f636d03185da72c
Pau Lòpez Pujals <iespaulopez@gmail.com>
2020-10-10 16:04:45 +0200

    Fix error

--- controllers/database.js
+++ controllers/database.js
@@ -1,8 +1,9 @@
 const dbFile = "./data/sqlite.db";
+const fs = require("fs");
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
-const fs = require("fs");
+
 
 module.exports = {
     setupDatabase(name, args){
