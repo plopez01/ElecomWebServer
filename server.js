17515dd254305f52e2b661f8d0324a59618d46bc
Pau Lòpez Pujals <iespaulopez@gmail.com>
2020-10-10 15:20:45 +0200

    Add mysqlite3

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
+const dbFile = "./data/sqlite.db";
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(dbFile);
 
-MongoClient.connect("mongodb://localhost:27017/ElecomWebServer", {useUnifiedTopology: true}, function(err, db) {
-  if(!err) {
-    console.log("We are connected");
+db.serialize(() =>{
+  if(!exists){
+    db.run("CREATE TABLE Users (id INTEGRER PRIMARY KEY AUTOINCREMENT, name TEXT, pskhash TEXT, salt TEXT)");
+    console.log("Creating Users Table...");
+  } else {
+    console.log("Users Database ready!");
   }
+  
 });
 
 app.get('/', function (req, res) {
