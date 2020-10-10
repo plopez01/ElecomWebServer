317d605f07732288ae701fe962ca698215c3417b
Pau Lòpez Pujals <iespaulopez@gmail.com>
2020-10-10 16:07:00 +0200

    Test

--- controllers/database.js
+++ controllers/database.js
@@ -4,7 +4,6 @@ const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
 
-
 module.exports = {
     setupDatabase(name, args){
         db.serialize(() =>{
